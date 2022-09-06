5b2320c24eb72152758dca9d3de5334b8bdb1eee
Jussi Palo <jussi@jussipalo.com>
2022-09-06 08:41:06 +0100

    Further fix to invalid first hour price

--- forecasts/nordpool.php
+++ forecasts/nordpool.php
@@ -102,8 +102,6 @@ function get_forecast_nordpool($redis,$params)
         if (isset($timevalues[$forecast_time])) {
             $value = $timevalues[$forecast_time];
             $last_time_value = $forecast_time;
-        } else if (isset($timevalues[$forecast_time+(3600)])) { // if not available, this may be feed that only shows forecasts starting from next hour
-            $value = $timevalues[$forecast_time+(3600)];
         } else {
             $value = null;
         }
@@ -111,8 +109,23 @@ function get_forecast_nordpool($redis,$params)
         $profile[] = $value == null ? null : 1*$value;
     }
     
+    if($profile[0]=null) {
+        $profile[0]="EMPTY";
+    }
+    if($profile[1]=null) {
+        $profile[1]="EMPTY";
+    }
+
     // remove empty array items
     $profile = array_filter($profile);
+
+    if($profile[0]="EMPTY") {
+        $profile[0]=null;
+    }
+    if($profile[1]="EMPTY") {
+        $profile[1]=null;
+    }
+
     // adjust end value accordingly
     $params->end = $last_time_value;
 
